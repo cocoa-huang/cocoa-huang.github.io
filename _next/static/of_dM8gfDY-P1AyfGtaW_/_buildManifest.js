d64039d29c547aef3308c2913ee71c42a41d9b0b
Eric Huang <zkhuang521@gmail.com>
2024-10-31 16:56:20 -0700

    Updates

---
+++ _next/static/of_dM8gfDY-P1AyfGtaW_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2cca2479-9628a09f8eaa4575.js","static/chunks/884-6ad3d30df607d315.js","static/chunks/pages/index-ea4d27e41c5387eb.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
